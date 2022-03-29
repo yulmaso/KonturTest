d2dd3c761cc94e18ba7dfa1f0e704083046339e2
Плотников Алексей Александрович <AAlPlotnikov@sberbank.ru>
2022-03-29 18:13:05 +0300

    Фикс билд ворнинга от рума, убрал темную тему

--- app/src/main/java/com/yulmaso/konturtest/data/database/AppDatabase.kt
+++ app/src/main/java/com/yulmaso/konturtest/data/database/AppDatabase.kt
@@ -6,9 +6,7 @@ import androidx.room.TypeConverters
 import com.yulmaso.konturtest.data.database.dao.ContactDao
 import com.yulmaso.konturtest.data.database.dto.ContactDbDto
 
-@Database(entities = [
-    ContactDbDto::class
-], version = 1)
+@Database(entities = [ContactDbDto::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun contactDao(): ContactDao
